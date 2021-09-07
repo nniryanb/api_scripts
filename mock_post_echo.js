9a32135e01fd9976d7b633003510be97eece964f
nn iryanb <90267997+nniryanb@users.noreply.github.com>
2021-09-07 08:30:30 -0700

    Update mock_post_echo.js

--- mock_post_echo.js
+++ mock_post_echo.js
@@ -10,15 +10,10 @@ import { check, sleep } from 'k6';
 export let options = {
 
   stages: [
-
     { duration: '30s', target: 200 },
-
     { duration: '3d', target: 400 },
-
     { duration: '20s', target: 100 },
-
   ],
-
 };
 
 export default function () {
